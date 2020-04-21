faad49b83ca0d2542e1684d27451b5c7da49882c
Alina <alina.4ertovski@gmail.com>
2020-04-21 17:10:07 +0300

    added files

---
+++ hw-03/task-2.js
@@ -0,0 +1,24 @@
+// Напиши функцию countProps(obj), считающую кол-во свойств в объекте.
+// Функция возвращает число - количество свойств.
+
+const countProps = function(obj) {
+
+  const keys = Object.keys(obj);
+
+  let total = 0;
+
+for (const key in keys) {
+    console.log(key);
+
+    total += 1;
+};
+
+console.log('Total: ', total);
+
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
